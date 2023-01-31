65512f01927677e33ee71afa03e99d3bdc30691a
cellva03 <selvavignesh432@gmail.com>
2023-01-31 12:47:08 +0530

    initial

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+var cors = require('cors')
+
+dotenv.config();
+app.use(cors());
+const PORT = process.env.PORT || 8800;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
+app.listen(PORT, () => {
+  console.log("Backend server is running!");
+});
